c0b2dafd5f15e98b4a23b8837cdb820a56c2b41d
Viktor Varga <vargavik@z52.hu>
2023-07-21 15:15:26 +0200

    230721

--- examples/wifi/getting_started/station/main/station_example_main.c
+++ examples/wifi/getting_started/station/main/station_example_main.c
@@ -99,6 +99,8 @@ void wifi_init_sta(void)
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
     ESP_ERROR_CHECK(esp_wifi_start() );
 
+    ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
+
     ESP_LOGI(TAG, "wifi_init_sta finished.");
 
     /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
